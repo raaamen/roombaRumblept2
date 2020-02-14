e61cdd43fe0e07f0b4231dfe62ce71d56ec958e1
sarah <52550982+raaamen@users.noreply.github.com>
2020-02-13 20:48:35 -0500

    no message

---
+++ RoombaRampagee/Assets/Scripts/showText.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showText : MonoBehaviour
+{
+
+    public int playersInGame;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void updateTextBoxes()
+    {
+        switch (playersInGame)
+        {
+            default:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            
+        }
+    }
+
+}
