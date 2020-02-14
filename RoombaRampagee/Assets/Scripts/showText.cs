b1fa0c924743b6f02338e7d7bfd316c14483ed6f
sarah <52550982+raaamen@users.noreply.github.com>
2020-02-13 20:58:30 -0500

    no message

--- RoombaRampagee/Assets/Scripts/showText.cs
+++ RoombaRampagee/Assets/Scripts/showText.cs
@@ -7,6 +7,11 @@ public class showText : MonoBehaviour
 
     public int playersInGame;
 
+    public GameObject player1text;
+    public GameObject player2text;
+    public GameObject player3text;
+    public GameObject player4text;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,15 +26,29 @@ public class showText : MonoBehaviour
 
     public void updateTextBoxes()
     {
+        player1text.SetActive(false);
+        player2text.SetActive(false);
+        player3text.SetActive(false);
+        player4text.SetActive(false);
         switch (playersInGame)
         {
-            default:
-                break;
             case 1:
+                player1text.SetActive(true);
                 break;
             case 2:
+                player1text.SetActive(true);
+                player2text.SetActive(true);
                 break;
             case 3:
+                player1text.SetActive(true);
+                player2text.SetActive(true);
+                player3text.SetActive(true);
+                break;
+            case 4:
+                player1text.SetActive(true);
+                player2text.SetActive(true);
+                player3text.SetActive(true);
+                player4text.SetActive(true);
                 break;
             
         }
